e4cda04c0543bc381f8b784cfac34205407d803d
tonghe <597673123@qq.com>
2017-09-30 15:45:42 +0800

    asdas

---
+++ resources/views/Company/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="utf-8">
+	<meta content="支付,囯银,微信,支付宝,扫码" name="keywords">
+	<meta content="聚成付" name="description">
+	<title>聚成付</title>
+	<link rel="stylesheet" href="/assets/ext/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/css/main.css">
+</head>
+<body>
+<div id="carousel-example-generic" class="carousel slide" data-ride="">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	</ol>
+
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner" role="listbox">
+		<div class="item active">
+			<img src="/assets/images/1481164132.jpg">
+			<div class="carousel-caption">
+				<h3>聚成付，想付就付!</h3>
+				<p>数科支付集成，让支付变得简单快捷</p>
+			</div>
+		</div>
+		<div class="item">
+			<img src="/assets/images/1481161992.jpg">
+			<div class="carousel-caption">
+				<h3>银行一清直连</h3>
+				<p>资金安全有保障</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Controls -->
+	<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
+<script src="/assets/js/jquery-2.2.3.min.js"></script>
+<script src="/assets/ext/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
